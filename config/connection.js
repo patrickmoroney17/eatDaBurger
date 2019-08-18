534c395de11862e034ca522e458fc6f056097849
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-18 14:46:52 -0400

    heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "eatDaBurger_db"
+  user: "gldptqjwcahqnj9k",
+  password: "eku7mq2tkpathss7",
+  database: "ezb1rxqrg63xnqg4"
 });
 
 // Make connection.
