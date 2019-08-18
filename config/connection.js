459570a2077a7a5903bde96321e195b1455e92d2
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-18 17:55:05 -0400

    restarting the heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "b3lns32qshskaajm",
-  password: "ap5p8h1t5gon5c6a",
-  database: "j2p0ctgiin4fq1cq"
-});
+// var connection = mysql.createConnection({
+//   host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com,
+//   port: 3306,
+//   user: "rykz1l0vw0wjxugx",
+//   password: "mh4c3iiq6jtbzihx",
+//   database: "vz9grcsvruy8yas0"
+// });
 
-// var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "password",
-//     database: "eatDaBurger_db"
-//   });
-// };
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "eatDaBurger_db"
+  });
+};
 
 
 
